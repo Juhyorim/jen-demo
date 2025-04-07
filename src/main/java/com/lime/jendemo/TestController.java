5c1b4791c6b45f9488ab96bf9829555ff7383ad4
Juhyorim <joohr1234@gmail.com>
2025-04-07 13:14:34 +0900

    fix: swagger 에러 수정

--- src/main/java/com/lime/jendemo/TestController.java
+++ src/main/java/com/lime/jendemo/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping
+    @GetMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("Hello World");
     }
